f63c88f69163b8d458f8f287cb24c61e45ef405f
spmason <steve@spmason.com>
2011-01-31 22:39:24 +0000

    Ready for presentation

--- src/N2Presentation/Models/Parts/TwitterPart.cs
+++ src/N2Presentation/Models/Parts/TwitterPart.cs
@@ -1,5 +1,6 @@
 using System;
 using N2;
+using N2.Details;
 using N2Presentation.Details;
 
 namespace N2Presentation.Models.Parts
@@ -7,8 +8,8 @@ namespace N2Presentation.Models.Parts
 	[PartDefinition("Twitter", SortOrder = 10, IconUrl = "~/Content/img/SocialBookmarks/Twitter_16x16.png")]
 	public class TwitterPart : PartBase
 	{
-		//[EditableTextBox("Username @", 20)]
-		[EditableTwitterUserName("Username @", 20)]
+		[EditableTextBox("Username @", 20)]
+		//[EditableTwitterUserName("Username @", 20)]
 		public virtual string UserName { get; set; }
 	}
 }
